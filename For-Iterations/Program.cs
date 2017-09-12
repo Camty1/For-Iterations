1beb92653d7d4e62dd8bb9c845d855a54e528d6d
Camty1 <cwolfe@vms.edu>
2017-09-12 13:46:40 -0600

    Work done
    :fire::fire::fire::fire::fire::fire::fire::fire:

---
+++ For-Iterations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) // 
+            {
+                //Console.WriteLine(i.ToString());
+
+                if (i == 7)
+                {
+                    Console.WriteLine("Found Seven");
+                    break; // Use this to exit from the for loop since you would waste processing going through the rest of the iterations of the for loop
+                    // Break is same as Js
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++) // Same as Javascript, just int instead of var
+            {
+                Console.WriteLine(myValue);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
